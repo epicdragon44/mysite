a4bffaacf584d1c5c8f87189857e97e693a88790
Daniel Wei <weidaniel15@gmail.com>
2023-06-25 02:51:25 -0500

    Add Landing Page

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Daniel Wei | Portfolio";
+export const SITE_DESCRIPTION = "A collection of my work and thoughts.";
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            cursor: {
+                semi: "url(/clickon.png), default",
+                solid: "url(/clickoff.png), pointer",
+            },
+        },
+    },
+    plugins: [require("@tailwindcss/typography")],
+};
