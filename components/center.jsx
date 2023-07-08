b75a1cbdbd3dad6c8a7de3378582bfcac74b9a94
Daniel Wei <weidaniel15@gmail.com>
2023-07-08 17:29:03 -0500

    Lol

--- components/center.jsx
+++ components/center.jsx
@@ -3,29 +3,52 @@ import CustomLink from "./link";
 // TODO: USE INTER EVERYWHERE
 
 const CenterSplash = () => (
-    <div>
-        <h1
+    <div
+        style={{
+            width: "100vw",
+            height: "100vh",
+            position: "absolute",
+            top: "0",
+            left: "0",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+        }}
+    >
+        <div
             style={{
-                fontSize: "4rem",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "start",
+                gap: "0",
             }}
         >
-            daniel wei
-        </h1>
-        <p>
-            developer & (occasional) designer <br />
-            leading development @{" "}
-            <CustomLink href='https://cornelldti.org' text='cornell dti' />
-            <br />
-            product development @{" "}
-            <CustomLink
-                href='https://www.instapathbio.com/'
-                text='instapath'
-            />{" "}
-            (yc w19) <br />
-            majoring in computer science @ cornell <br />
-            minoring in ui/ux & east asian studies
-            <br />
-        </p>
+            <h1
+                style={{
+                    fontSize: "4rem",
+                    margin: "0",
+                }}
+            >
+                daniel wei
+            </h1>
+            <p>
+                developer & (occasional) designer <br />
+                leading development @{" "}
+                <CustomLink href='https://cornelldti.org' text='cornell dti' />
+                <br />
+                product development @{" "}
+                <CustomLink
+                    href='https://www.instapathbio.com/'
+                    text='instapath'
+                />{" "}
+                (yc w19) <br />
+                majoring in computer science @ cornell <br />
+                minoring in ui/ux & east asian studies
+                <br />
+            </p>
+        </div>
     </div>
 );
 
